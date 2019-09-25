eebf358959d6a780b1c10cee04de5c0ad8ce3353
ike3 <ike3@server.org>
2019-09-25 15:58:55 +0300

    [IMMERSIVE] Fix typos in configuration parameters

--- ImmersiveConfig.cpp
+++ ImmersiveConfig.cpp
@@ -22,11 +22,11 @@ bool ImmersiveConfig::Initialize()
 
     serverPort = config.GetIntDefault("Immersive.ServerPort", 0);
     manualAttributes = config.GetBoolDefault("Immersive.ManualAttributes", true);
-    sharedXpPercent = config.GetFloatDefault("Immersive.sharedXpPercent", 90.0f);
-    sharedRepPercent = config.GetFloatDefault("Immersive.sharedRepPercent", 90.0f);
+    sharedXpPercent = config.GetFloatDefault("Immersive.SharedXpPercent", 90.0f);
+    sharedRepPercent = config.GetFloatDefault("Immersive.SharedRepPercent", 90.0f);
     sharedQuests = config.GetBoolDefault("Immersive.SharedQuests", true);
     fishingBaubles = config.GetBoolDefault("Immersive.FishingBaubles", true);
-    sharedPercentRaceRestiction = config.GetIntDefault("Immersive.SharedPercentRaceRestiction", 2);
+    sharedPercentRaceRestiction = config.GetIntDefault("Immersive.SharedPercentRaceRestriction", 2);
     sharedPercentMinLevel = config.GetFloatDefault("Immersive.SharedPercentMinLevel", 5);
     attributeLossPerDeath = config.GetIntDefault("Immersive.AttributeLossPerDeath", 1);
     fallDamageMultiplier = config.GetFloatDefault("Immersive.FallDamageMultiplier", 1);
