093dc3271de0c2cd8c4b06925ccce3ed9579f24b
ike3 <ike3@server.org>
2021-11-03 09:22:48 +0300

    [IMMERSIVE] party portals fix

--- sql/immersive_world_portals.sql
+++ sql/immersive_world_portals.sql
@@ -11,7 +11,14 @@ INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `condition_id`) 
 SELECT menu_id, 71, 5, 'Open portal for my party', 18, 16, 0, 40, 0, 0, 0, '', 0 FROM `gossip_menu_option` where menu_id in (4822,4823,4827,4821,4825,4826);
 
--- add missing gossips to trainers
--- update creature_template set GossipMenuId = 4537 where entry in (select entry from creature_template_portal_backup);
+INSERT INTO `gossip_menu_option`
+(`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `condition_id`) 
+SELECT entry, 71, 5, 'Open portal for my party', 18, 16, 0, 40, 0, 0, 0, '', 0 FROM `gossip_menu` where entry in (4822,4823,4827,4821,4825,4826) and 
+not exists (SELECT * FROM `gossip_menu_option` where menu_id = entry and id = 71);
+
+INSERT INTO `gossip_menu_option`
+(`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `condition_id`) 
+SELECT entry, 0, 3, 'Please teach me.', 5,	16,	0,	0,	0,	0,	0,	'', 0 FROM `gossip_menu` where entry in (4822,4823,4827,4821,4825,4826) and 
+not exists (SELECT * FROM `gossip_menu_option` where menu_id = entry and id = 0);
 
 SELECT * FROM creature_template where SubName = 'Portal Trainer';
